fe9e537e20d60779a81a91b1088782153b39aa49
jatiinyadav <jatin27yadav@gmail.com>
2022-01-17 21:30:14 +0530

    Programs

---
+++ CQuestions/Temperature.java
@@ -0,0 +1,16 @@
+package CQuestions;
+
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args){
+        Scanner inputTemp = new Scanner(System.in);
+
+        float tempinC = inputTemp.nextFloat();
+
+        float tempinF = (tempinC * 1.8f) + 32;
+        inputTemp.close();
+
+        System.out.println("Temp in F is: " + tempinF);
+    }
+}

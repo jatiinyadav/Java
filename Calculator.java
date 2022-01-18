60fbd6e96183256e3de039a038e6aeb6a8ee9dfe
jatiinyadav <jatin27yadav@gmail.com>
2022-01-19 00:03:01 +0530

    Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,13 +2,37 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            int ans = 0;
 
-        int ans = 0;
+            while(true){
 
-        while(true){
+                System.out.print("Enter operator: ");
+                char op = input.next().trim().charAt(0);
 
-            input.close();
+                if(op == '+' || op == '-' || op == '/'){
+                    
+                    int n1 = input.nextInt();
+                    int n2 = input.nextInt();
+
+
+                    if(op == '+'){
+                        ans = n1 + n2;
+                    }
+                    if(op == '-'){
+                        ans = n1 - n2;
+                    }
+                    if(op == '/'){
+                        ans = n1 / n2;
+                    }
+                } else{
+                    System.out.println("Incorrect Operator");
+                    break;
+                }
+
+                System.out.println(ans);
+                
+            }
         }
     }
 }

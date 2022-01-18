f888bb9f112368b869d3179e42b4da9f720ddc78
jatiinyadav <jatin27yadav@gmail.com>
2022-01-18 23:46:56 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int ans = 0;
+
+        while(true){
+
+            input.close();
+        }
+    }
+}

b2ae8070fa129d48eb7e61f1ad854af1189e09ca
jatiinyadav <jatin27yadav@gmail.com>
2022-02-06 23:38:23 +0530

    Questions

---
+++ Leetcode/Easy/ConcatArray.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/concatenation-of-array/
+
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+
+//         int[] concat = new int[nums.length + nums.length];
+
+//         int k = nums.length;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             concat[i] = nums[i];
+//             concat[k] = nums[i];
+//             k++;
+//         }
+
+//         return concat;
+
+//     }
+// }

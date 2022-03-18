80df5f2a8339e1923499bff2c520d2e10bb7fc59
jatiinyadav <jatin27yadav@gmail.com>
2022-03-18 21:38:30 +0530

    Leetcode Questions & String

--- Practice.java
+++ Practice.java
@@ -1,30 +1,12 @@
 public class Practice {
     public static void main(String[] args) {
 
-        String name = null;
-        System.out.println(check(name));
-    }
-
-    static String check(String name) {
-
-        if(name == null){
-            return "Not a Palindrone";
-        }
-
-        int start = 0;
-        int end = name.length() - 1;
-
-        while (start <= end) {
-
-            if (name.charAt(start) == name.charAt(end)) {
-                start++;
-                end--;
-            } else {
-                return "Not a Palindrone";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.println("*");
             }
         }
-
-        return "A Palindrone";
-
+        
     }
+
 }

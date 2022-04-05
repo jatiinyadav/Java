2cd6afe8c2ad72ed74fe2ad39ebcb4a5dc7e5b82
jatiinyadav <jatin27yadav@gmail.com>
2022-04-05 23:30:49 +0530

    Second Largest Number

--- Practice.java
+++ Practice.java
@@ -3,17 +3,23 @@ import java.util.Arrays;
 public class Practice {
     public static void main(String[] args) {
 
-        int[][] arr = { { -1, 3, 4 }, { 8, 4 }, { 8, 2 } };
+        String series = "";
 
-        for (int i = 0; i <= arr.length - 1; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j <= arr[i].length - 1; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.print("]");
-            System.out.println();
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            series += ch;
         }
 
+        System.out.println(series);
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i <= 25; i++){
+            char ch = (char)('a'+ i);
+            builder.append(ch);
+        }
+
+        System.out.println(builder);
     }
 
 }

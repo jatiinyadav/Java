89c1e823afbd523406e0450783dcc561a3d479d2
jatiinyadav <jatin27yadav@gmail.com>
2022-04-21 23:56:59 +0530

    Leetcode

--- Practice.java
+++ Practice.java
@@ -3,11 +3,17 @@ import java.util.Arrays;
 public class Practice {
     public static void main(String[] args) {
 
-        int[] arr1 = { 1, 2 };
-        int[] arr2 = { 4, 2, 5 };
-        
-        int[] smaller = arr1.length > arr2.length ? arr2 : arr1;
-        System.out.println(Arrays.toString(smaller));
+        int[] arr1 = { 10, 2, 5, 3};
 
+        for (int i = 0; i <= arr1.length - 1; i++){
+            for (int j = 0; j <= arr1.length - 1; j++){
+                if(arr1[i] == arr1[j] * 2){
+                    System.out.println("Yes");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No");
     }
 }

5ee220cc60523aa474558d57978344133e3590ea
jatiinyadav <jatin27yadav@gmail.com>
2022-05-19 17:52:39 +0530

    Folders Arranged

--- Practice.java
+++ Practice.java
@@ -1,19 +1,21 @@
-import java.util.Arrays;
-
-class Test{
-    static String name;
-
-    Test(String name){
-        Test.name = name;
-    }
-}
-
 public class Practice {
     public static void main(String[] args) {
 
-        Test ex = new Test("Hello");
-        System.out.println(Test.name);
+        int num = 14;
+        System.out.println(numSteps(num, 0));
 
+    }
+
+    static int numSteps(int num, int steps){
+        if(num == 0){
+            return steps;
+        }
+
+        if(num % 2 == 0){
+            return numSteps(num / 2, steps + 1);
+        }
+
+        return numSteps(num - 1, steps + 1);
     }
 
 }

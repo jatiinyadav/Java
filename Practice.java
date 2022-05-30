39a050cb6db4ff93d6923c8e7e0581ae5f874623
jatiinyadav <jatin27yadav@gmail.com>
2022-05-30 23:49:22 +0530

    Fibonacci

--- Practice.java
+++ Practice.java
@@ -3,27 +3,17 @@ import java.util.List;
 
 public class Practice {
     public static void main(String[] args) {
-
-        String tiles = "AAABBC";
-        ArrayList<String> resp = combination(tiles, "");
-        System.out.println(resp);
-        System.out.println(resp.size() - 1);
+        int ans = fibonacci(5);
+        System.out.println(ans);
     }
 
-    static ArrayList<String> combination(String title, String ans) {
-
-        if (title.isEmpty()) {
-            ArrayList<String> res = new ArrayList<>();
-            res.add(ans);
-            return res;
+    static int fibonacci(int size) {
+        if (size <= 1) {
+            return size;
+        } else {
+            int f = fibonacci(size - 1) + fibonacci(size - 2);
+            return f;
         }
-
-        char c = title.charAt(0);
-        ArrayList<String> left = combination(title.substring(1), ans + c);
-        ArrayList<String> right = combination(title.substring(1), ans);
-
-        left.addAll(right);
-        return left;
     }
 
 }

d1443f973bd35e5335f6dd8076b9afda4c355c41
jatiinyadav <jatin27yadav@gmail.com>
2022-05-31 23:52:47 +0530

    Backtracking

--- Practice.java
+++ Practice.java
@@ -1,19 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Practice {
     public static void main(String[] args) {
-        int ans = fibonacci(5);
-        System.out.println(ans);
-    }
 
-    static int fibonacci(int size) {
-        if (size <= 1) {
-            return size;
-        } else {
-            int f = fibonacci(size - 1) + fibonacci(size - 2);
-            return f;
-        }
     }
 
 }

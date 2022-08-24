5d223becab4f094b11a2baabfc743f7cd9e22960
jatiinyadav <jatin27yadav@gmail.com>
2022-08-24 23:51:54 +0530

    Questions

--- Exam.java
+++ Exam.java
@@ -1,27 +1,60 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Exam {
     public static void main(String[] args) {
 
-        String ans = reverseWords("i.like.this.program.very.much");
-        System.out.println(ans);
-    }
+        int[] arr = { 2, 1, 3, 6, 5, 4 };
 
-    static String reverseWords(String S) {
-        String[] arr = S.split("\\.");
+        mergeSort(arr, 0, arr.length - 1);
 
         System.out.println(Arrays.toString(arr));
 
-        String ans = "";
+    }
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            ans += arr[i];
-            if (i > 0)
-                ans += ".";
+    static void mergeSort(int[] arr, int start, int end) {
+        if (end - start == 1) {
+            return;
         }
 
-        return ans;
+        int mid = start + (end - start) / 2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+
+        merge(arr, start, mid, end);
     }
+
+    static void merge(int[] arr, int start, int mid, int end) {
+        int[] ans = new int[end - start];
+
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        while (i < mid && j < end) {
+            if (arr[i] < arr[j]) {
+                ans[k] = arr[i];
+                i++;
+            } else {
+                ans[k] = arr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < mid) {
+            ans[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < end) {
+            ans[k] = arr[j];
+            j++;
+            k++;
+        }
+
+    }
+
 }

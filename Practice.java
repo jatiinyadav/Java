a877399a6caa157df2f88e3184433e5c46a77f0f
jatiinyadav <jatin27yadav@gmail.com>
2022-08-25 23:56:26 +0530

    Questions

--- Practice.java
+++ Practice.java
@@ -1,45 +1,37 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Practice {
-    public static void main(String[] args) {
+    public static ArrayList<Integer> countEleLessThanOrEqual(int arr1[], int arr2[], int m, int n) {
+        Map<Integer, Integer> mp = new HashMap<>();
 
-        int[][] matrix = { { 1, 10, 4, 2 }, { 9, 3, 8, 7 }, { 15, 16, 17, 12 } };
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        int[] col = new int[matrix[0].length];
-        int[] row = new int[matrix.length];
+        for (int i = 0; i < arr2.length; i++) {
+            mp.put(arr2[i], mp.getOrDefault(arr2[i], 0) + 1);
+        }
 
-        min(matrix, row, col);
+        int count = 0;
 
-        System.out.println(Arrays.toString(row));
-        System.out.println(Arrays.toString(col));
-
-    }
-
-    public static void min(int[][] matrix, int[] row, int[] col) {
-
-        int ind1 = 0;
-        int ind2 = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for (int j = 0; j < matrix[0].length; j++) {
-                min = Math.min(min, matrix[i][j]);
-                max = Math.max(max, matrix[j][i]);
+        for (int j = 0; j < arr1.length; j++) {
+            count = 0;
+            for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+                if (entry.getKey() <= arr1[j]) {
+                    count += mp.getOrDefault(entry.getKey(), 0);
+                }
             }
-            row[ind1++] = min;
-            col[ind2++] = max;
+            ans.add(count);
         }
 
+        return ans;
     }
 
-    public static void max(int[][] matrix, int j) {
+    public static void main(String[] args) {
+        
+        int[] arr1 = {1, 2, 3, 4, 7, 9};
+        int[] arr2 = {0, 1, 2, 1, 1, 4};
 
-        int minim = Integer.MIN_VALUE;
-
-        for (int row = j; row < matrix.length; row++) {
-            minim = Math.max(minim, matrix[row][j]);
-        }
+        ArrayList<Integer> res = countEleLessThanOrEqual(arr1, arr2, 2, 2);
+        System.out.println(res);
 
     }
 }

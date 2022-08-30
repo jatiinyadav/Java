f463a9e44230b0ad1b02085d109ef18d6ed89e41
jatiinyadav <jatin27yadav@gmail.com>
2022-08-30 23:44:44 +0530

    Questions

--- Exam.java
+++ Exam.java
@@ -1,59 +1,5 @@
-import java.util.Arrays;
-
 public class Exam {
     public static void main(String[] args) {
-
-        int[] arr = { 2, 1, 3, 6, 5, 4 };
-
-        mergeSort(arr, 0, arr.length - 1);
-
-        System.out.println(Arrays.toString(arr));
-
-    }
-
-    static void mergeSort(int[] arr, int start, int end) {
-        if (end - start == 1) {
-            return;
-        }
-
-        int mid = start + (end - start) / 2;
-
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid, end);
-
-        merge(arr, start, mid, end);
-    }
-
-    static void merge(int[] arr, int start, int mid, int end) {
-        int[] ans = new int[end - start];
-
-        int i = start;
-        int j = mid;
-        int k = 0;
-
-        while (i < mid && j < end) {
-            if (arr[i] < arr[j]) {
-                ans[k] = arr[i];
-                i++;
-            } else {
-                ans[k] = arr[j];
-                j++;
-            }
-
-            k++;
-        }
-
-        while (i < mid) {
-            ans[k] = arr[i];
-            i++;
-            k++;
-        }
-
-        while (j < end) {
-            ans[k] = arr[j];
-            j++;
-            k++;
-        }
 
     }
 
--- Practice.java
+++ Practice.java
@@ -1,52 +1,36 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Practice {
     public static void main(String[] args) {
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] arr = { 935, 1231, 91 };
+        int sum = 0;
+        int ans = 0;
 
-        arr.add(2);
-        arr.add(4);
-        arr.add(3);
-        arr.add(5);
-        arr.add(1);
+        for (int i = 0; i < arr.length; i++) {
 
-        int ind = 0;
+            sum = 0;
 
-        int j = 1;
-
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        int k = 2;
-        while (j <= k) {
-
-            int number = 0;
-            if (arr.get(ind) > arr.get(ind + 1)) {
-                number = arr.remove(ind + 1);
-                arr.add(number);
-                mp.put(arr.get(ind), mp.getOrDefault(arr.get(ind), 0) + 1);
-            } else {
-                number = arr.remove(ind);
-                arr.add(number);
-                mp.put(arr.get(ind + 1), mp.getOrDefault(arr.get(ind + 1), 0) + 1);
+            while (arr[i] > 0) {
+                int last = arr[i] % 10;
+                arr[i] /= 10;
+                sum += last;
             }
 
-            j++;
-        }
-
-        int maximum = 0;
-        int number = 1;
-
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            if (mp.getOrDefault(entry.getKey(), 0) > maximum) {
-                maximum = mp.getOrDefault(entry.getValue(), 0);
-                number = entry.getKey();
+            int flag = 0;
+            while (sum > 9) {
+                int last = sum % 10;
+                sum /= 10;
+                ans += last;
+                flag = 1;
             }
+
+            if (flag == 1) {
+                ans += sum;
+            }
+
+            ans = Math.max(ans, sum);
         }
 
-        System.out.println(number);
-        int num;
+        System.out.println(ans);
+
     }
 }

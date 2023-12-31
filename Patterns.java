74d1bd05e0cb911ffffd9cd9c8e07eaf17c88a74
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2023-12-31 19:12:30 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -22,5 +22,73 @@ class HelloWorld {
             }
             System.out.println();
         }
+
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+        for (int i = 1; i <=5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // 1
+        // 22
+        // 333
+        // 4444
+        // 55555
+        for (int i = 1; i <=5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+        for (int i = 0; i <=4; i++){
+            for(int j = 0 ; j <= 4-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 12345
+        // 1234
+        // 123
+        // 12
+        // 1
+       for (int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 6-i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+        int l = (5 * 2 - 1)/2;
+		for (int i = 1; i <= 5; i++){
+            
+            for(int k = 0; k < l; k++){
+                System.out.print(" ");
+            }
+            
+            for(int j = 1; j < i * 2; j++){
+                System.out.print("*");
+            }
+            
+            l--;
+            System.out.println();
+        }
     }
 }

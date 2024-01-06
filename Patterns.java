226e170fd6985e914a6f742ca7c24f3dd25ea65f
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2024-01-07 00:10:13 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -131,5 +131,23 @@ class HelloWorld {
 	// 1         1
 	// 1 2     2 1
 	// 1 2 3 3 2 1
+	        int spaces = n * 2 - 2;
+		
+		for(int i = 0; i < n; i++){
+		    for(int j = 0; j <= i; j++){
+		        System.out.print(j + 1 + " ");
+		    }
+		    
+		    for(int k = 0; k < spaces; k++){
+		        System.out.print(' ');
+		    }
+		    
+		    for(int j = i; j >= 0; j--){
+		        System.out.print(j + 1 + " ");
+		    }
+		    
+		    spaces = spaces - 2;
+		    System.out.println();
+		}
     }
 }

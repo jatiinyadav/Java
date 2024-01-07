09a0b666b8cc8d8f2f71a0daafab97b9040dc6d7
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2024-01-07 15:39:49 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -189,5 +189,24 @@ class HelloWorld {
 		    
 		    System.out.println();
 		}
+ 	// 4444444
+	// 4333334
+	// 4322234
+	// 4321234
+	// 4322234
+	// 4333334
+	// 4444444
+	int n = 4;
+        for(int i = 0; i < n * 2 - 1; i++){
+            for(int j = 0; j < n * 2 - 1; j++){
+                int top = i;
+                int left = j;
+                int right = n * 2 - 2 - j;
+                int bottom = n * 2 - 2 -i;
+                
+                System.out.print(n -  Math.min(Math.min(top, left), Math.min(right, bottom)));
+            }
+            System.out.println();
+        }
     }
 }
